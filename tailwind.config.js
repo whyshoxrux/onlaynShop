f7df06f53096fae3b0d820011c8fea1323166a23
whyshoxrux <tohirovshohruxbek@gmail.com>
2024-12-11 12:54:36 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+    },
+  },
+  plugins: [flowbite],
+};
